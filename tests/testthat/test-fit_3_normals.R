7eed2c4606d8074922ef6561622db4d90b795776
stathismegas <stathismegas@gmail.com>
2023-03-17 11:45:13 +0000

    test default is activated in case of bad convergence

--- tests/testthat/test-fit_3_normals.R
+++ tests/testthat/test-fit_3_normals.R
@@ -1,4 +1,4 @@
-
+# simulate scRNA reads
 hops = 500
 soups = 100
 cells = 100
@@ -6,11 +6,10 @@ mean_hops = 7
 mean_soups = 300
 mean_cells = 1300
 atac_downsampling = 0.8
-
 nCount = round(c(  rnorm(hops, mean=mean_hops, sd=1), rnorm(soups, mean=mean_soups, sd=50) , rnorm(cells, mean=mean_cells, sd=150) ))
 out = fit_3_normals(nCount)
 
-
+# simulate scATAC reads
 hops = 500
 soups = 100
 cells = 100
@@ -40,32 +39,20 @@ test_that("ATAC: barhop_end is between the mean of barhops and the mean of ambie
 
 
 
-# # more overlapping Gaussian
-# 
-# hops = 1000
-# soups = 100
-# cells = 100
-# mean_hops = 1
-# mean_soups = 35
-# mean_cells = 1300
-# # hops = 5000
-# # soups = 1000
-# # cells = 1000
-# atac_downsampling = 1
-# 
-# nCount = round(c(  rnorm(hops, mean=mean_hops, sd=10), rnorm(soups, mean=mean_soups, sd=30) , rnorm(cells, mean=mean_cells, sd=150) ))
-# out_atac = fit_3_normals_atac(nCount)
-# 
-# equil = equil_of_normals(0.539105692069073, 10.8688383006814, 0.714113582232385, 31.6969572827669, 10.3549610529109, 0.142857142857143)
-# equil
-# 
-# equil = equil_of_normals(100, 10, 0.5, 200, 10, 0.5)
-# test_that("", {
-#   expect_equal(equil, 150)
-# })
-# equil = equil_of_normals(100,10.1,0.5, 200, 10, 0.5)
-# test_that("", {
-#   expect_equal(equil, 150)
-# })
+# Cause bad convergence in ATAC
+hops = 1
+soups = 1
+cells = 1
+max_hops = 10
+max_soups = 50
+mean_cells = 20000
+nCount_atac = round(c(  runif(hops, min=0, max=max_hops), runif(soups, min=max_hops, max=max_soups) , rnorm(cells, mean=mean_cells, sd=150) ))
+out_atac = fit_3_normals_atac(nCount_atac)
+test_that("in case of bad convergence test default parameters are activated", {
+  expect_equal( out_atac[1], 160 )
+  expect_equal( out_atac[2], 4.625116 )
+})
+
+
 
 
